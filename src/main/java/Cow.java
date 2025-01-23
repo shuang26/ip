cab9d5ad7e2b7c52c8e0ec2f6ab8bcc094558e98
shuang26 <shuangyuan9@gmail.com>
2025-01-23 16:47:30 +0800

    Level-0

---
+++ src/main/java/Cow.java
@@ -0,0 +1,12 @@
+public class Cow {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+        + "Hello! I'm Cow\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________\n";
+
+        System.out.println(logo);
+    }
+}
