59170075a6e1824110291117806b90b1452fca6c
shuang26 <shuangyuan9@gmail.com>
2025-02-01 15:31:29 +0800

    Add Storage and TaskList classes to seperate responsibilities

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,32 +1,41 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Cow {
+    private Storage storage;
+    private TaskList tasks;
     private TaskManager taskManager;
     private Ui ui;
 
-    public Cow() {
+    public Cow(String filePath) {
         this.ui = new Ui();
-    }
+        this.storage = new Storage(filePath);
+        this.tasks = storage.loadTasksFromFile();
 
-    public void start() {
         // Initialise Task Manager and Load task from saved file / create new file
         this.taskManager = new TaskManager();
         taskManager.start();
+    }
 
+    public void run() {
         ui.showWelcome();
         Scanner sc = new Scanner(System.in);
+        boolean isExit = false;
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine().trim();
-
-            if (input.isEmpty()) continue;
-            if (taskManager.handleInput(input)) break;
+            if (input.isEmpty()) {
+                continue;
+            }
+            if (taskManager.handleInput(input)) {
+                break;
+            }
         }
         sc.close();
     }
 
     public static void main(String[] args) {
-        Cow cow = new Cow();
-        cow.start();
+        Cow cow = new Cow("data/cow.txt");
+        cow.run();
     }
 }
